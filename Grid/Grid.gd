7efb3816d8d2fd04c29e42d227e1d8480daa0ce3
ZrbTz <thomasmadeo@outlook.com>
2021-12-24 13:17:25 +0100

    Working minesweeper

--- Grid/Grid.gd
+++ Grid/Grid.gd
@@ -17,8 +17,11 @@ func draw_grid():
 		for h in range(height):
 			var block = block_scene.instance()
 			block.position = block.scale*block.sprite_size*Vector2(w, h)
+			block.coordinates = Vector2(w, h)
 			add_child(block)
 			tiles[Vector2(w, h)] = block
+	for b in tiles.values():
+		b.neighbours = map_pos_to_tile(get_neighbours(b.coordinates))
 			
 func init_bombs():
 	var bombs = 0
@@ -27,20 +30,34 @@ func init_bombs():
 		var rw = abs(randi() % width)
 		randomize()
 		var rh = abs(randi() % height)
-		var tile = tiles[Vector2(rw, rh)]
+		var pos = Vector2(rw, rh)
+		var tile = tiles[pos]
 		if not tile.has_bomb:
 			tile.has_bomb = true
 			bombs += 1
-			add_neighbours(rw, rh)
+			add_neighbours(pos)
 			
-func add_neighbours(rw, rh):
-	for v2 in get_neighbours(rw, rh):
-		if(tiles.has(v2)):
-			tiles[v2].bomb_counter+=1
+func add_neighbours(pos):
+	for v2 in get_neighbours(pos):
+		tiles[v2].bomb_counter+=1
 	
-func get_neighbours(rw, rh):
-	return [Vector2(rw-1, rh), Vector2(rw-1, rh-1), Vector2(rw-1, rh+1), Vector2(rw+1, rh),
+func get_neighbours(v2):
+	var rw = v2.x
+	var rh = v2.y
+	var list = [Vector2(rw-1, rh), Vector2(rw-1, rh-1), Vector2(rw-1, rh+1), Vector2(rw+1, rh),
 	Vector2(rw+1, rh+1), Vector2(rw+1, rh-1), Vector2(rw, rh-1), Vector2(rw, rh+1)]
+	var result_list = []
+	for v2 in list:
+		if(tiles.has(v2)):
+			result_list.append(v2)
+	return result_list
+	
+func map_pos_to_tile(pos_list):
+	var tile_list = []
+	for pos in pos_list:
+		tile_list.append(tiles[pos])
+	return tile_list
+			
 
 func bombed():
 	emit_signal("got_bomb")
--- Grid/GridBlock/GridBlock.gd
+++ Grid/GridBlock/GridBlock.gd
@@ -5,6 +5,8 @@ var state = BlockState.COVERED
 var sprite_size = 256
 var has_bomb: bool = false
 var bomb_counter = 0
+var coordinates: Vector2
+var neighbours: Array
 #signal got_bomb
 
 # Called when the node enters the scene tree for the first time.
@@ -17,14 +19,14 @@ func _on_Control_gui_input(event):
 	match(state):
 		BlockState.COVERED:
 			if event.is_action_pressed("left_click"):
+				state = BlockState.PRESSED
 				$Cover.hide()
 				if has_bomb:
 					$Bomb.show()
 #					GameManager.lose()
 					get_parent().bombed()
 				else:
-					$Label.text = str(bomb_counter)
-				state = BlockState.PRESSED
+					uncover()
 			if event.is_action_pressed("right_click"):
 				$Flag.show()
 				state = BlockState.FLAGGED
@@ -32,4 +34,18 @@ func _on_Control_gui_input(event):
 			if event.is_action_pressed("right_click"):
 				$Flag.hide()
 				state = BlockState.COVERED
+				
+func uncover_all():
+	for n in neighbours:
+		if n.state == BlockState.COVERED:
+			if not n.has_bomb:
+				n.uncover()
+			
+func uncover():
+	state = BlockState.PRESSED
+	$Cover.hide()
+	if(bomb_counter > 0):
+		$Label.text = str(bomb_counter)
+	elif bomb_counter == 0: 
+		uncover_all()
 
